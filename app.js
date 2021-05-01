6a972ffb29cc12501ef6bcd859586f77c7e637a8
talharulez <talharulez85@gmail.com>
2021-05-01 07:52:56 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,322 @@
+
+// chapter 1
+// task 1
+
+// alert("welcome to our website");
+
+
+// chapter 1 
+// task 2
+// alert("Error! Please enter a valid password.");
+
+
+// chapter1 
+// task3
+// // alert("Welcome to JS Land...\nHappy Coding!");
+
+// chapter 1
+// task4
+// alert("Welcome to JS Land");
+// alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
+
+// chapter 1
+// task 5
+// alert("Hello... I can run JS through my web browser's console");
+
+// chapter 1
+// task 6
+
+// alert ("Hello World , this is my program");
+
+
+// chapter 1
+// task 7 
+
+// d. (Body after your page's HTML)
+
+// chapter 2
+// task 1
+// var name= "username";
+// alert(name);
+
+// chapter 2
+// task 2
+// var myName= "talha";
+
+// chapter 2
+// task3
+//  var message ="Hello World";
+//  alert(message);
+
+// chapter 2
+// task 4
+// var name="John Doe" ;
+// alert(name);
+// var age="15 years old";
+// alert(age);
+// var z="Certified Mobile Applicaton Development";
+// alert(z);
+
+//  chapter 2
+//  task 5
+//  var x="PIZZA\nPIZZ\nPIZ\nPI\nP";
+//  alert(x);
+
+// chapter2
+// task 6
+// var email="My Email Adress is ";
+// var adress="example@example.com";
+// alert(email + adress);
+
+//  chapter 2
+//  task7
+//  var book="I am trying to learn from the book  A Smarter \n Way to learn Java Script";
+//  alert(book);
+
+
+//  chapter 2 
+//  task 8. 
+
+//  document.write("yah! I can write html content through javascript");
+
+// chapter 2
+// task 9
+// var a = ("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬");
+// alert (a);
+
+// chapter 3
+
+// task 1
+// var age= ("my age is 15 years old");
+// alert(age);
+
+// chapter 3
+// task 2
+
+// var visit=("you have visited this site 14 times");
+// alert(visit);
+
+// chapter 3
+// task 3
+
+// var birthYear = 1990;
+// document.write("my birth year is 1990 <br> Data type of my declared variable is number");
+
+// chapter 3 
+// task 4 
+
+// 
+
+// chapter 4
+// task1
+
+// var a= "hello world";
+// var b= "talha";
+// var c= 30;
+
+// chapter 4 
+// task 2
+
+// 5 legal variable name are letter ,underscore, digit and dollar sign, camel case.
+// 5 illegal variable , space is ilegal in variable ,%, use of underscore in start of the variable
+
+// chapter 4
+// task3 
+
+//   document.write("<b>Rules for naming JS variables </b><br><br><br><br>");
+
+
+
+// document.write("variable names can only contain number,$ and _ for example: $my_1stvariable <br>"  );
+// document.write("variable must begin with a letter ,$ or _. for example $name, _name and name<br>");
+// document.write("variable name are case sensitive <br>");
+// document.write("variable name should not be JS keyword");
+
+// chapter 5
+// task1
+
+// var x = 3;
+// var y = 5;
+// var z = x + y;
+// alert(z);
+
+// chapter 5 task 2
+
+// var x = 3;
+// var y = 5;
+// var z = x - y;
+// alert(z);
+
+
+// var x = 3;
+// var y = 5;
+// var z = x * y;
+// alert(z);
+
+// var x = 3;
+// var y = 5;
+// var z = x / y;
+// alert(z);
+
+
+
+// var x = 3;
+// var y = 5;
+// var z = x % y;
+// alert(z);
+
+// chapter 5
+// task 4
+
+// var ticket = 600;
+// var cost = 5 ;
+// var totalCost = ticket * cost ;
+// alert (totalCost);
+// document.write("Total Cost to buy 5 tickets to a movie is 3000PKR");
+
+
+// chapter 5 task 5
+
+// var table =prompt ("please enter any number");
+
+// for (var a = 1; a<=10; a++ ){
+//     document.write(table + " * " + a + " = " + (a*table) + "</br>");
+// }
+
+
+// chapter 5 task 6
+
+
+
+//  var f=prompt("enter fehrenheit");
+//  alert((f-32)/1.8);
+
+
+// var c=prompt("enter celcius");
+// alert((c*9/5)+32);
+
+// chapter 5 task 7
+
+
+//  document.write("<b>Shopping Cart</b> <br><br><br><br>") ;
+
+
+// document.write("price of item 1 is 650 <br>");
+// document.write("quantity of item 1 is 3<br>");
+// document.write("price of item 2 is 100<br>");
+// document.write("quantity of item 2 is 7<br>");
+// document.write("shipping charges 100<br><br>");
+// document.write("Total cost of your order is 2750")
+
+//  var item1 = 650;
+//  var item2 = 100;
+//  var qty1 = 3;
+//  var qty2 = 7;
+//  var sCost = 100;
+//  cost = (item1 * qty1) + (item2 * qty2) + sCost;
+//  alert(cost);
+
+
+// chapter 5 task 8
+
+
+// document.write("<b>Marks Sheet</b> <br><br><br><br>");
+// document.write("Total Marks : 980<br>");
+// document.write("Marks obtained: 804<br>");
+// document.write("pecentage: 82.0408163265306 %");
+
+
+
+
+
+// var totalMarks = 980;
+// var marksObtained = 804;
+// var perCentage = marksObtained/totalMarks*100;
+// // alert(perCentage+ "%");
+
+// chapter 5 task 9
+
+
+
+// var usDollar = 10;
+// var saRiyal = 25;
+// var usDolvalue = 104.8;
+// var saRiyalvalue = 28;
+// var currencyinPkr = (usDollar * usDolvalue) + (saRiyal * saRiyalvalue);
+// alert(currencyinPkr);
+
+
+// document.write("<b>Currency in Pkr</b> <br><br><br><br>");
+// document.write("Total Currency in PKR : 1748")
+
+// chapter 5 task 10
+
+// var a = 5;
+// var b = 5;
+// var c = (b+ 5) + (b * 10) + (b/10);
+// alert(c);
+
+
+// chapter 5 task 11
+
+// var currrentYear = 2021;
+// var birthYear = 1990 ;
+// var totalAge = currrentYear - birthYear ;
+// alert(totalAge);
+
+// chapter 5 task 12
+
+// var radius = 3;
+// var circumference = Math.PI * 2*radius;
+// alert("The circumference is " + circumference);
+// var area = Math.PI * radius*radius;
+// alert("The area is " + area);
+
+// chapter 5 task 13
+
+// var age = 28;
+// var maxAge = 65;
+// var numPerDay = 3;
+// var totalNeeded = (numPerDay * 365) * (maxAge - age);
+// var message = 'You will need ' + totalNeeded + ' meal to last you until the ripe old age of ' + maxAge;
+// alert(message);
+
+
+// chapter 6-9 task 1
+
+
+//   var a = 10 ;
+//   var b= ++a + ++a + --a + a--;
+//   var c= alert(b);
+
+// chapter 6-9 task 2
+
+//  var a = 2;
+//  var b = 1;
+//  var result = --a + --b + ++b + b--;
+// alert(result);
+
+// value of a is 1;
+// value of b is 1;
+// result is 3 ;
+
+// chapter 6-9 task 3
+
+// var welCome= prompt("welcome to my place , thank you");
+
+// chapter 6-9 task 5
+
+
+// var num = prompt("Enter Number", "0") 
+// var num = 5;
+
+
+// chapter 11 task 1
+
+// if (city = "karachi");{
+//    city =prompt("welcome the city of light");
+
+// }
+
+// chapter 11 task 2
+
